4b5c5af3cc83d5d5677660da2f25cadbccacf791
David Gonzalez <davgonz15559@gmail.com>
2025-09-19 00:20:44 -0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/David-s-portfolio/', // Replace 'engineering-portfolio' with your actual repository name
+  base: '/David-s-portfolio', // Replace 'engineering-portfolio' with your actual repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
